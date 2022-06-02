e76d08865d80f9384a2a21bf873ee85d92ef68ee
Nikolaj Geisle <niko737@edu.ucl.dk>
2022-06-02 11:25:58 +0200

    remove --nobuild

--- create_slicing_filter_condition.ps1
+++ create_slicing_filter_condition.ps1
@@ -17,7 +17,7 @@ Set-Location "$location/tests"
 $newDir = Get-ChildItem
 Write-Host "NewDir: $newDir"
 Set-Location "Umbraco.Tests.Integration"
-$tests = dotnet test --no-build --list-tests # search for test files with specific pattern.
+$tests = dotnet test --list-tests # search for test files with specific pattern.
 $totalAgents = [int]$Env:SYSTEM_TOTALJOBSINPHASE # standard VSTS variables available using parallel execution; total number of parallel jobs running
 $agentNumber = [int]$Env:SYSTEM_JOBPOSITIONINPHASE  # current job position
 $testCount = $tests.Count
